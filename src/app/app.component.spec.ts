e524a6e311e615e9a8d831c913e16fcf2f1ee849
Rácz Zalán <racz1666@gmail.com>
2022-06-16 01:50:24 +0200

    basic project configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'JapanJegyzetek'`, () => {
+  it(`should have as title 'Japan-Jegyzetek'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('JapanJegyzetek');
+    expect(app.title).toEqual('Japan-Jegyzetek');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('JapanJegyzetek app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Japan-Jegyzetek app is running!'
+    );
   });
 });
+
